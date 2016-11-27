6e772ef0b83740c28e5f8f4643d063823ceb4737
Hong <pnxhong@gmail.com>
2016-11-27 17:31:03 +0100

    Implement LunchCheck

---
+++ module1-solution/app.js
@@ -0,0 +1,30 @@
+(function(){
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunCheckController);
+
+  LunCheckController.$inject = ['$scope'];
+  function LunCheckController($scope){
+    $scope.lunchMenu = "";
+
+    $scope.checkIfTooMuch = function(){
+      if(!$scope.lunchMenu){
+        $scope.message = 'Please enter data first';
+      } else{
+        var items = $scope.lunchMenu.split(',');
+        var itemCount = 0;
+        for(var i = 0; i < items.length; i++){
+          if(items[i]){
+            itemCount++;
+          }
+        }
+        if(itemCount <=3){
+          $scope.message = 'Enjoy!';
+        } else{
+          $scope.message = 'Too much!';
+        }
+      }
+    };
+  };
+})();
